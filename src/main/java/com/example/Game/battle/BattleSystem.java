ae0cb250a2ca5c118f67a74c1ecfa67c34acba46
Dmitry <dmitrykulikov99@gmail.com>
2025-08-15 18:46:34 +0300

    custom class BattleSystem

--- src/main/java/com/example/Game/battle/BattleSystem.java
+++ src/main/java/com/example/Game/battle/BattleSystem.java
@@ -1,5 +1,20 @@
 package com.example.Game.battle;
 
-public class BattleSystem {
+import com.example.Game.character.GameCharacter;
+import com.example.Game.character.Ork;
 
+public class BattleSystem {
+    public void Fight (GameCharacter character_1, GameCharacter character_2) {
+        System.out.println("The fight begins! " + character_1.getRace() + " vs " + character_2.getRace());
+
+        int round = 1;
+        while (character_1.getHealth()>0 && character_2.getHealth()>0){
+
+
+        }
+    }
+
+    public void attack(){
+
+    }
 }
