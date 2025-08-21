b4f722832a6524ffe737d2f3140f99201b083043
Dmitry <dmitrykulikov99@gmail.com>
2025-08-21 18:56:54 +0300

    in class battlesystem add explanations

--- src/main/java/com/example/Game/battle/BattleSystem.java
+++ src/main/java/com/example/Game/battle/BattleSystem.java
@@ -42,11 +42,11 @@ public class BattleSystem {
         if ( weapon == null ){ // если у персонажа нет оружия
             System.out.println(attacker_1.getRace() + " hasn't weapon and punch with 5 damage ");
             attacker_2.takeDamage(5);
-            return; // ?????????
+            return; // это выход из метода
         }
 
         int baseDamage = weapon.getDamage();
-        int strengthBonus = attacker_1.getStrength() / 2;   // ????????
+        int strengthBonus = attacker_1.getStrength() / 2;   // Эта строка вычисляет бонус к урону от силы персонажа **
         int totalDamage = baseDamage + strengthBonus;
 
         System.out.println(attacker_1.getRace() + " attack with " + weapon.getName() + " !");
@@ -56,3 +56,12 @@ public class BattleSystem {
         attacker_2.takeDamage(totalDamage);
     }
 }
+
+// ** Потому что:
+//Мы не хотим, чтобы сила слишком сильно влияла на урон.
+//Иначе сильный персонаж будет наносить слишком много урона, и игра станет несбалансированной
+
+/* Например: У персонажа сила = 90
+        Оружие наносит 70 урона
+        Если добавить всю силу: 70 + 90 = 160 → слишком много
+        Если добавить половину: 70 + 45 = 115 → разумно */
